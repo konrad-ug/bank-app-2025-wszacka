68dbf285b7c1fd8b36681fad8509c9b820538c6d
Weronika Szacka <wszacka@inf.ug.edu.pl>
2025-10-16 15:12:50 +0200

    feature 1 i 2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe",79010288773)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 0.0
+        assert account.pesel == 79010288773
